960ad48d8eda8644050648b6774801d979f7417e
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-10 17:03:58 +0300

    feat: Bootstrap with default styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,11 @@ import { withProviders } from "./providers";
 import { Routing } from "../pages";
 
 import "./styles/index.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App position-absolute vh-100 w-100 bg-primary">
       <Routing />
     </div>
   );
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const TestPage = (props: Props) => {
-  return <div>Test</div>;
+  return <div className="container">Test</div>;
 };
 
 export default TestPage;
