d7e642820b3d190313701ee862fd994ff9ebb227
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-19 22:15:23 +0300

    feat: Menu link to profile

--- src/entities/user/ui/Menu/Menu.tsx
+++ src/entities/user/ui/Menu/Menu.tsx
@@ -3,6 +3,7 @@ import { useLogout } from "entities/auth";
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -12,7 +13,9 @@ export const Menu: React.FC<Props> = () => {
   const { logout } = useLogout();
   return (
     <DropdownButton id="dropdown-basic-button" title="Меню" align="end">
-      <Dropdown.Item href="/profile">Профиль</Dropdown.Item>
+      <Dropdown.Item as={Link} to="/profile">
+        Профиль
+      </Dropdown.Item>
       <Dropdown.Divider />
       <Dropdown.Item onClick={logout}>Выйти</Dropdown.Item>
     </DropdownButton>
