62353e7ee5eedbf42ee815f5682fe14a15c04682
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-20 15:33:29 +0300

    fix: Home page text

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,40 @@ const Home = (props: Props) => {
   return (
     <div className="container">
       <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam adipisci
+        iure ipsum voluptatum voluptatibus velit, officiis debitis cum earum
+        illum cupiditate distinctio minus sequi dolore quidem hic soluta
+        expedita tempora? Earum natus quibusdam ut deleniti repellat esse facere
+        nesciunt autem alias corporis quas cupiditate suscipit culpa dicta illum
+        ullam nemo tempore, asperiores velit. Tempore quas vero aliquam
+        recusandae cum fugit. Incidunt autem cumque voluptatibus maiores aliquam
+        praesentium rerum reprehenderit deserunt, iste vitae architecto quae
+        repellendus harum explicabo odio animi magni! Error ducimus dolores
+        cumque non, culpa ad minima eligendi alias. Magni, doloribus ullam vero
+        quis placeat quos, quasi aliquid sunt ipsa repellat amet est ut quas cum
+        expedita blanditiis sit iste voluptatibus eligendi explicabo voluptates
+        nisi iusto et? Tempore, dignissimos. Quod fugit, dolor distinctio harum
+        ducimus excepturi alias ea recusandae aliquam culpa repudiandae fuga
+        libero possimus dolore iure aliquid ipsum, eveniet eius vel! Magni nemo
+        amet fuga aliquam ab. Ea? Odit neque dignissimos non placeat minus
+        asperiores enim culpa recusandae aut praesentium, autem quam aperiam!
+        Maiores optio ipsa, natus id laboriosam, cum libero, quos recusandae
+        assumenda deserunt est consectetur temporibus! Eius reiciendis ad quidem
+        harum, quisquam doloremque sunt, libero veniam, officia eveniet
+        voluptatibus possimus ratione? Aliquid, perferendis. Omnis, amet
+        voluptatibus a facilis officiis culpa provident libero, sint doloribus
+        iste sapiente. Saepe corrupti consectetur esse aspernatur, culpa eum
+        ipsam consequatur amet facere quisquam iure dignissimos nobis pariatur
+        similique recusandae ab a iusto adipisci illo debitis facilis obcaecati
+        dicta, laudantium at. Vero. Eius quia consectetur harum ipsa. Quas nam
+        similique maiores! Et veniam aperiam consequatur minus autem fugit earum
+        ducimus a officia, accusamus quisquam explicabo perferendis, animi
+        facilis enim iste unde delectus! Soluta esse maxime ab doloremque
+        tenetur mollitia. Quas vitae maxime, aspernatur voluptate ipsum nam
+        optio. Voluptatem tempora inventore dolor, laudantium consectetur quo,
+        sit consequuntur voluptatum eaque at adipisci saepe officiis.
+      </p>
     </div>
   );
 };
